52b09e0cf3e036e7df9de5fa55dfdc4487170790
Janelle Fanoga <fanogaajanelle@gmail.com>
2026-03-07 11:49:56 +0800

    Inheritance

--- src/main/java/org/example/service/StudentRegistration.java
+++ src/main/java/org/example/service/StudentRegistration.java
@@ -1,4 +1,5 @@
 package org.example.service;
+
 import org.example.model.Student;
 import java.util.ArrayList;
 
@@ -10,32 +11,38 @@ public class StudentRegistration {
     }
 
     public void displayAllStudent() {
+        if (studentList.isEmpty()) {
+            System.out.println("No student records found.");
+            return;
+        }
         for (Student s : studentList) {
             System.out.println(s.toString());
         }
     }
 
 
-    public void updateStudent(String id, String newName, String newProgram) {
-        for (int i = 0; i < studentList.size(); i++) {
-            if (studentList.get(i).getStudentID().equals(id)) {
-                studentList.get(i).setStudentName(newName);
-                studentList.get(i).setProgram(newProgram);
+    public void updateStudent(int id, String newName, String newProgram) {
+        for (Student s : studentList) {
+
+            if (s.getId() == id) {
+                s.setName(newName);
+                s.setProgram(newProgram);
                 System.out.println("Update successful!");
                 return;
             }
         }
-        System.out.println("Student not found.");
+        System.out.println("Student with ID " + id + " not found.");
     }
 
 
-    public void removeStudent(String id) {
+    public void removeStudent(int id) {
         for (int i = 0; i < studentList.size(); i++) {
-            if (studentList.get(i).getStudentID().equals(id)) {
+            if (studentList.get(i).getId() == id) {
                 studentList.remove(i);
-                System.out.println("Student removed.");
+                System.out.println("Student with ID " + id + " removed.");
                 return;
             }
         }
+        System.out.println("Remove failed: Student not found.");
     }
 }
